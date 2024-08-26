9a02270bd4b710737bfdf6037b816f3ac3d3aa83
Sam Pitcher <sampitcher@google.com>
2024-08-26 13:34:54 +0200

    try again

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"
-
-@app.route('/api/hello', methods=['POST'])
-def hello():
-    data = request.get_json()
-    return jsonify({"message": "Hello, " + data.get('name', 'World')})
+def hello_world():
+    return jsonify(message='Hello, World!')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
